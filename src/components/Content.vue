7a2b19c7d34523d912ead43f67f662b955882eb6
金铭 鲁 <JinmingMaster@outlook.com>
2019-08-13 00:04:53 +0800

    move the control-aspect and search to header

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="content">
-    <div class="control-aspect">
-      <div class="aspect">
-        <a-radio-group v-model="aspect">
-          <a-radio-button value="a">外观</a-radio-button>
-          <a-radio-button value="b">材质</a-radio-button>
-          <a-radio-button value="c">功能</a-radio-button>
-        </a-radio-group>
-        <a-button @click="handleGen" style="margin-left: 10px">文案生成</a-button>
+<!--     
+    <div class="control-aspect" >
+        <div class="aspect">
+          <a-radio-group v-model="aspect">
+            <a-radio-button value="a">外观</a-radio-button>
+            <a-radio-button value="b">材质</a-radio-button>
+            <a-radio-button value="c">功能</a-radio-button>
+          </a-radio-group>
+          <a-button @click="handleGen" style="margin-left: 10px">文案生成</a-button>
+        </div>
       </div>
-    </div>
-    <div class="control-search">
-      <a-select mode="tags" style="width: 80%;" @change="handleChange" placeholder="请输入关键词">
-        <a-select-option v-for="i in keywords" :key="i">{{i}}</a-select-option>
-      </a-select>
-      <a-button @click="handleGen" slot="suffix" class="search-btn" type="primary">
-        <a-icon type="smile" spin />
-      </a-button>
-    </div>
+
+      <div class="control-search">
+        <a-select mode="tags" style="width: 80%;" @change="handleChange" placeholder="请输入关键词">
+          <a-select-option v-for="i in keywords" :key="i">{{i}}</a-select-option>
+        </a-select>
+        <a-button @click="handleGen" slot="suffix" class="search-btn" type="primary">
+          <a-icon type="smile" spin />
+        </a-button>
+      </div> -->
+
     <a-tabs defaultActiveKey="1" @change="callback">
       <a-tab-pane tab="Mode 1" key="1">
         <modeone ref="modeone" />
@@ -33,6 +36,7 @@
 
 <script>
 /* eslint-disable */
+import bus from "./Bus.js"
 import Basemode from "./Basemode.vue"
 import Advancemode from './Advancemode.vue';
 const API = "http://deecamp.tangkailh.cn:10081/";
@@ -52,24 +56,20 @@ export default {
   },
   mounted() {
     this.LoadKeyword();
+    bus.$on("params_in", (val)=>{
+      let params=val 
+      window.test = this.$refss
+      this.$refs.modeone.reqGenDoc(params)
+      this.$refs.modetwo.reqGenSen(params)
+    })
   },
   methods: {
     LoadKeyword() {
       this.$http.get(API + "deecamp_keywords").then(res => {
-        // console.log(res.data);
         this.keywords = res.data.slice(0, 100);
       });
     },
-    handleChange(value) {
-      // console.log(`selected ${value}`);
-      this.queryWords = value;
-    },
-    handleGen() {
-      let params = "keywords="+this.queryWords.join(" ")+"&aspects="+this.aspect
-      window.test = this.$refss
-      this.$refs.modeone.reqGenDoc(params)
-      this.$refs.modetwo.reqGenSen(params)
-    }
+    
   },
   filters: {
     capitalize: function(value) {
@@ -89,12 +89,7 @@ export default {
 /* div {
   margin: 10px;
 } */
-.control-aspect {
-  margin: 0 0 20px 0;
-}
-.control-search {
-  margin: 0 0 20px 0;
-}
+
 .control-res {
   margin: 0 0 20px 0;
   overflow: hidden;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,19 +3,59 @@
 
     <div class="header-item">
       
-      <img src="../assets/logowhite.png" style="height: 70%; margin: auto; float: left"/>
+      <img src="../assets/logowhite.png" style="height: 80%; margin: auto; float: left"/>
+
+      <div class="control-aspect" >
+        <div class="aspect">
+          <a-radio-group v-model="aspect">
+            <a-radio-button value="a">外观</a-radio-button>
+            <a-radio-button value="b">材质</a-radio-button>
+            <a-radio-button value="c">功能</a-radio-button>
+          </a-radio-group>
+          <a-button @click="handleGen" style="margin-left: 10px">文案生成</a-button>
+        </div>
+      </div>
+
+      <div class="control-search">
+        <a-select mode="tags" style="width: 80%;" @change="handleChange" placeholder="请输入关键词">
+          <a-select-option v-for="i in keywords" :key="i">{{i}}</a-select-option>
+        </a-select>
+        <a-button @click="handleGen" slot="suffix" class="search-btn" type="primary">
+          <a-icon type="smile" spin />
+        </a-button>
+      </div>
+    
     </div>
   </div>
 </template>
 
 <script>
 // import logo from './../assets/logo.png'
-
+import bus from './Bus.js'
 export default {
   name: "Header",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      queryWords:"",
+      aspect: "a",
+      keywords: []
+    }
+  },
+  methods:{
+    handleChange(value){
+      this.queryWords = value;
+    },
+
+    handleGen(){
+      let params = "keywords="+this.queryWords.join(" ")+"&aspects="+this.aspect
+      bus.$emit("params_in", params)
+    }
   }
+
+
 };
 </script>
 
@@ -34,6 +74,17 @@ export default {
   color: grey;
   font-size: large;
 }
+.control-aspect {
+  /* margin: 0 0 20px 0; */
+  height: 70%;
+  float:right
+}
+.control-search {
+  height: 100%;
+  width:  70%;
+  /* margin: 0 0 0 70px; */
+}
+
 ul {
   list-style-type: none;
   padding: 0;
