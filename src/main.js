0e9216d2589ee2587418d1c546483b14bb461803
SWTangKai <tangkailh@outlook.com>
2019-08-13 12:27:41 +0800

    add mode1

--- src/main.js
+++ src/main.js
@@ -6,10 +6,16 @@ import 'ant-design-vue/dist/antd.less'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+// import VueAwesomeSwiper from 'vue-awesome-swiper'
+// import 'swiper/dist/css/swiper.css'
+
+
 Vue.config.productionTip = false
 
 Vue.use(Antd)
 Vue.use(VueAxios, axios)
+
+// Vue.use(VueAwesomeSwiper);
 
 new Vue({
   el: '#app',
